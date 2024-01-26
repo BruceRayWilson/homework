37f0b18fed62345c623db02353d2a7a7a5747176
Bruce Ray Wilson <BruceRayWilson42@gmail.com>
2024-01-26 16:24:53 -0700

    Have roll data for training/test.

--- GenRollData.py
+++ GenRollData.py
@@ -11,28 +11,24 @@ class GenRollData:
 
         for i in range(10):
             modified_data = self.data.copy()
-            
-            # Adding offset to the 'Time (s)' column
             time_offset = 40 * i
             modified_data['Time (s)'] += time_offset
 
-            # Multiplying the 'Roll (deg)' column by a random factor for copies after the first
-            if i > 0:
-                roll_factor = np.random.uniform(0.95, 1.05)
-                modified_data['Roll (deg)'] *= roll_factor
+            # Applying multiplier to the 'Roll (deg)' column
+            roll_multiplier = np.random.uniform(0.95, 1.05) if i > 0 else 1.0
+            modified_data['Roll (deg)'] *= roll_multiplier
 
-            # Append the modified copy to the list
+            # Adding a multiplier column
+            modified_data['Multiplier'] = roll_multiplier
+
             all_copies.append(modified_data)
 
         # Concatenate all copies into a single DataFrame
         combined_data = pd.concat(all_copies, ignore_index=True)
 
-        # Saving the combined data to a single file 'roll.csv'
+        # Saving the combined data to 'roll.csv'
         combined_data.to_csv('roll.csv', index=False)
 
-# Assuming that the 'filtered_roll.csv' file is available in the same directory
-# Create an instance of the class and call exec()
+# Example usage
 gen_roll_data = GenRollData('filtered_roll.csv')
 gen_roll_data.exec()
-
-# This code will generate a single file 'roll.csv' containing all modified copies of the data.
